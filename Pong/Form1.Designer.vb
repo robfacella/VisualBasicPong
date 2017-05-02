103e20ca19423341ee2b50f363311071f4762708
Rob Face <Rob Face>
2017-05-02 16:55:41 -0400

    Last edit 1/31/2012

---
+++ Pong/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Player = New System.Windows.Forms.PictureBox()
+        Me.CPU = New System.Windows.Forms.PictureBox()
+        Me.Ball = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CPU, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Ball, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Player
+        '
+        Me.Player.BackColor = System.Drawing.Color.White
+        Me.Player.Location = New System.Drawing.Point(12, 128)
+        Me.Player.Name = "Player"
+        Me.Player.Size = New System.Drawing.Size(25, 170)
+        Me.Player.TabIndex = 0
+        Me.Player.TabStop = False
+        '
+        'CPU
+        '
+        Me.CPU.BackColor = System.Drawing.Color.White
+        Me.CPU.Location = New System.Drawing.Point(1087, 128)
+        Me.CPU.Name = "CPU"
+        Me.CPU.Size = New System.Drawing.Size(25, 170)
+        Me.CPU.TabIndex = 1
+        Me.CPU.TabStop = False
+        '
+        'Ball
+        '
+        Me.Ball.BackColor = System.Drawing.Color.White
+        Me.Ball.Location = New System.Drawing.Point(462, 166)
+        Me.Ball.Name = "Ball"
+        Me.Ball.Size = New System.Drawing.Size(25, 25)
+        Me.Ball.TabIndex = 2
+        Me.Ball.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(24, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(1088, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 25)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 20
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1142, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(68, 63)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Classic"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1347, 20)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 63)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Caldwell"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1142, 91)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(67, 57)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Green"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1348, 89)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(67, 57)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Speed"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(1348, 163)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(67, 57)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Hide Ball"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(1142, 167)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(67, 57)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "Red"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1124, 462)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Ball)
+        Me.Controls.Add(Me.CPU)
+        Me.Controls.Add(Me.Player)
+        Me.Name = "Form1"
+        Me.Text = "Pong"
+        CType(Me.Player, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CPU, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Ball, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Player As System.Windows.Forms.PictureBox
+    Friend WithEvents CPU As System.Windows.Forms.PictureBox
+    Friend WithEvents Ball As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+
+End Class
---
+++ Pong/Form1.vb
@@ -0,0 +1,144 @@
+﻿Public Class Form1
+
+    Dim speed As Single = 10
+
+    Dim rndInst As New Random()
+
+    Dim xVel As Single = Math.Cos(rndInst.Next(5, 10)) * speed
+
+    Dim yVel As Single = Math.Sin(rndInst.Next(5, 10)) * speed
+
+    Dim CPUScore As Integer = 0
+
+    Dim PlayerScore As Integer = 0
+
+    Private Sub Timer1_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        If Ball.Location.Y > 5 And Ball.Location.Y < Me.Height - 40 - Player.Height Then
+
+            CPU.Location = New Point(CPU.Location.X, Ball.Location.Y - 10)
+
+        End If
+
+        Ball.Location = New Point(Ball.Location.X + xVel, Ball.Location.Y + yVel)
+
+        If Ball.Location.Y < 0 Then
+
+            Ball.Location = New Point(Ball.Location.X, 0)
+            yVel = -yVel
+
+        End If
+
+
+        If Ball.Location.Y > Me.Height - Ball.Size.Height - 45 Then
+
+            Ball.Location = New Point(Ball.Location.X, Me.Height - Ball.Size.Height - 45)
+            yVel = -yVel
+
+        End If
+
+
+        If Ball.Bounds.IntersectsWith(Player.Bounds) Then
+
+            Ball.Location = New Point(Player.Location.X + Ball.Size.Width, Ball.Location.Y)
+            xVel = -xVel
+
+        End If
+
+
+        If Ball.Bounds.IntersectsWith(CPU.Bounds) Then
+
+            Ball.Location = New Point(CPU.Location.X - CPU.Size.Width - 1, Ball.Location.Y)
+            xVel = -xVel
+
+        End If
+
+        If Ball.Location.X < 0 Then
+
+            CPUScore = CPUScore + 1
+            Ball.Location = New Point(Me.Size.Width / 2, Me.Size.Height / 2)
+            Label2.Text = CPUScore
+
+        End If
+
+        If Ball.Location.X > Me.Width - Ball.Size.Width - Player.Width Then
+
+            PlayerScore = PlayerScore + 1
+            Ball.Location = New Point(Me.Size.Width / 2, Me.Size.Height / 2)
+            Label1.Text = PlayerScore
+
+        End If
+
+        'resize the screen + have cpu paddle stay near right boundary
+        'CPU.Location = New Point(, CPU.Location.Y - 10)
+
+    End Sub
+
+
+    Private Sub Form1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove
+
+        If e.Y > 5 And e.Y < Me.Height - 40 - Player.Height Then
+
+            Player.Location = New Point(Player.Location.X, e.Y)
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Player.BackColor = Color.White
+        CPU.BackColor = Color.White
+        Ball.BackColor = Color.White
+        Me.BackColor = Color.Black
+        Label1.ForeColor = Color.White
+        Label2.ForeColor = Color.White
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        Player.BackColor = Color.Blue
+        CPU.BackColor = Color.Blue
+        Ball.BackColor = Color.Blue
+        Me.BackColor = Color.White
+        Label1.ForeColor = Color.Blue
+        Label2.ForeColor = Color.Blue
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        Player.BackColor = Color.LimeGreen
+        CPU.BackColor = Color.LimeGreen
+        Ball.BackColor = Color.LimeGreen
+        Me.BackColor = Color.Black
+        Label1.ForeColor = Color.LimeGreen
+        Label2.ForeColor = Color.LimeGreen
+
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+
+        xVel = xVel + 25
+        yVel = yVel + 25
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+
+        Ball.BackColor = Color.Empty
+
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+
+        Player.BackColor = Color.Red
+        CPU.BackColor = Color.Red
+        Ball.BackColor = Color.Red
+        Me.BackColor = Color.Black
+        Label1.ForeColor = Color.Red
+        Label2.ForeColor = Color.Red
+
+    End Sub
+End Class
